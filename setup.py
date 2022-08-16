559efb2eda70ecf630603654443c49987163035d
Haichao Zhang <hczhang1@gmail.com>
2022-08-15 23:46:44 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,16 @@ from setuptools import find_packages
 setup(
     name='d4rl',
     version='1.1',
-    install_requires=['gym',
-                      'numpy',
-                      'mujoco_py',
-                      'pybullet',
+    install_requires=[
+                      #'gym',
+                      #'numpy',
+                      #'mujoco_py',
+                      #'pybullet',
                       'h5py',
                       'termcolor',  # adept_envs dependency
-                      'click',  # adept_envs dependency
-                      'dm_control' if 'macOS' in platform() else
-                      'dm_control @ git+https://github.com/deepmind/dm_control@main#egg=dm_control',
+                      #'click',  # adept_envs dependency
+                      #'dm_control' if 'macOS' in platform() else
+                      #'dm_control @ git+https://github.com/deepmind/dm_control@main#egg=dm_control',
                       'mjrl @ git+https://github.com/aravindr93/mjrl@master#egg=mjrl'],
     packages=find_packages(),
     package_data={'d4rl': ['locomotion/assets/*',
